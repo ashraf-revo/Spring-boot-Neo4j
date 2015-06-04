9ec7c653593c8314747c57927ed820f8c4656510
ashraf <ashraf1abdelrasool@gmail.com>
2015-06-04 11:04:15 +0300

    iniihi

--- src/main/java/org/revo/Config/Neo4jConfig.java
+++ src/main/java/org/revo/Config/Neo4jConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -21,7 +22,8 @@ public class Neo4jConfig extends Neo4jConfiguration {
 
     @Bean
     public GraphDatabaseService graphDatabaseService() {
-        return new GraphDatabaseFactory().newEmbeddedDatabase(storeDirectory);
+        return new SpringRestGraphDatabase("http://localhost:7474/db/data/","revo","revo");
+//        return new GraphDatabaseFactory().newEmbeddedDatabase(storeDirectory);
     }
 
 }
